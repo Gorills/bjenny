5d5e9784bb0fc9071414164f9c28ee6cd48f6911
lexx <lexx@aidriller.com>
2021-02-27 16:32:53 +0700

    change response "new" controller

--- catalog/controller/product/new.php
+++ catalog/controller/product/new.php
@@ -4,6 +4,8 @@ class ControllerProductNew extends Controller {
         // Optional. This calls for your language file
         $this->load->language('product/new');
 
+        $this->url->link('product/new');
+
         // Optional. Set the title of your web page
         $this->document->setTitle($this->language->get('heading_title'));
 
@@ -38,7 +40,54 @@ class ControllerProductNew extends Controller {
             'limit' => 10,
         );
 
-        $data['products'] = $this->model_catalog_product->getProducts($filter_data);
+        $results = $this->model_catalog_product->getProducts($filter_data);
+
+        foreach ($results as $result) {
+            if ($result['image']) {
+                $image = $this->model_tool_image->resize($result['image'], $this->config->get('theme_' . $this->config->get('config_theme') . '_image_product_width'), $this->config->get('theme_' . $this->config->get('config_theme') . '_image_product_height'));
+            } else {
+                $image = $this->model_tool_image->resize('placeholder.png', $this->config->get('theme_' . $this->config->get('config_theme') . '_image_product_width'), $this->config->get('theme_' . $this->config->get('config_theme') . '_image_product_height'));
+            }
+
+            if ($this->customer->isLogged() || !$this->config->get('config_customer_price')) {
+                $price = $this->currency->format($this->tax->calculate($result['price'], $result['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
+            } else {
+                $price = false;
+            }
+
+            if ((float)$result['special']) {
+                $special = $this->currency->format($this->tax->calculate($result['special'], $result['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
+            } else {
+                $special = false;
+            }
+
+            if ($this->config->get('config_tax')) {
+                $tax = $this->currency->format((float)$result['special'] ? $result['special'] : $result['price'], $this->session->data['currency']);
+            } else {
+                $tax = false;
+            }
+
+            if ($this->config->get('config_review_status')) {
+                $rating = (int)$result['rating'];
+            } else {
+                $rating = false;
+            }
+
+            $data['products'][] = array(
+                'product_id'  => $result['product_id'],
+                'thumb'       => $image,
+                'name'        => $result['name'],
+                'description' => utf8_substr(trim(strip_tags(html_entity_decode($result['description'], ENT_QUOTES, 'UTF-8'))), 0, $this->config->get('theme_' . $this->config->get('config_theme') . '_product_description_length')) . '..',
+                'price'       => $price,
+                'special'     => $special,
+                'tax'         => $tax,
+                'minimum'     => $result['minimum'] > 0 ? $result['minimum'] : 1,
+                'rating'      => $result['rating'],
+                'href'        => $this->url->link('product/product', 'path=' . $this->request->get['path'] . '&product_id=' . $result['product_id'])
+            );
+        }
+
+//        echo('<pre>'.print_r($data['products'], true).'</pre>');
         // Load the template file and show output
         $this->response->setOutput($this->load->view('product/new', $data));
     }
